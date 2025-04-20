b380d184299a30de88660b2add60ba61f0a13d3d
Felipe Paes da Silva <felipe.silva638@fatec.sp.gov.br>
2025-04-20 19:22:49 -0300

    ADM_aprovacao

---
+++ onglink/src/app/ADM_aprovacao/page.tsx
@@ -0,0 +1,20 @@
+import Header_ADM_aprovacao from "@/src/app/components/header_ADM_aprovacao"
+
+
+const ADM_aprovacao = () => {
+
+    return(
+        <>
+        <Header_ADM_aprovacao/>
+
+        <h1>teste</h1>
+
+
+        
+        </>
+    )
+
+
+};
+
+export default ADM_aprovacao;
---
+++ onglink/src/app/components/header_ADM_aprovacao.jsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from 'next/link';
+import Image from 'next/image';
+import logo_onglink_01 from '@/src/app/img/LOGO_ONGLINK_1.png'
+
+
+export default function HeaderCadastro(){
+    return(
+
+        <header className="bg-header ">
+          <div className="mt-0 pt-4 pb-4 pl-8">
+            <Image
+              src={logo_onglink_01} // Caminho relativo à pasta public
+              alt="Logo ONGLink"
+              width={130} // Largura em pixels
+              height={130} // Altura em pixels
+              className="shadow-md border border-white"
+            />
+          </div>
+      </header>
+
+
+
+
+    )
+}
