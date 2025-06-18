beba8f25773bdcd7e733c2725902f0fd9159a15f
Felipe Paes da Silva <felipe.silva638@fatec.sp.gov.br>
2025-06-17 22:00:18 -0300

    adm_aprovação alterações

--- onglink/src/app/ADM_aprovacao/page.tsx
+++ onglink/src/app/ADM_aprovacao/page.tsx
@@ -32,38 +32,46 @@ export default function ADM_aprovacao() {
 
         <Header_ADM_aprovacao/>
         <main>
-                <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Nº da Solicitação</th>
-          <th>Tipo da Solicitação</th>
-          <th>Nome da ONG/Empresa</th>
-          <th>Nome do Responsável</th>
-          <th>Data da Solicitação</th>
-          <th>CPF</th>
-          <th>CEP</th>
-          <th>CNPJ</th>
-        </tr>
-      </thead>
-      <tbody>
-        {products.map(
-            (product) => {
-                
-                return (
-                    <tr>
-                        <td>{product.num}</td>  
-                        <td>{product.tipo}</td>
-                        <td>{product.nome}</td>
-                        <td>{product.nomeResp}</td>
-                        <td>{product.Data}</td>
-                        <td>{product.CPF}</td>
-                        <td>{product.CEP}</td>
-                        <td>{product.CNPJ}</td>
-                    </tr>
-                );
-            })}
-      </tbody>
-    </Table>
+            <div className="border-2 rounded w-full p-1 border-gray-600  mt-3 mb-3 ">
+                <Table striped bordered hover >
+                    <thead className="border-2 rounded  ">
+                        <tr>
+                            <th>Nº da Solicitação</th>
+                            <th>Tipo da Solicitação</th>
+                            <th>Nome da ONG/Empresa</th>
+                            <th>Nome do Responsável</th>
+                            <th>Data da Solicitação</th>
+                            {/*<th>CPF</th>*/}
+                            {/*<th>CEP</th>*/}
+                            {/*<th>CNPJ</th>*/}
+                            <th>Aprovar?</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody className="border-2 rounded  ">
+                        {products.map(
+                            (product) => {
+                                
+                                return (
+                                    <tr>
+                                        <td>{product.num}</td>  
+                                        <td>{product.tipo}</td>
+                                        <td>{product.nome}</td>
+                                        <td>{product.nomeResp}</td>
+                                        <td>{product.Data}</td>
+                                        {/*<td>{product.CPF}</td>*/}
+                                        {/*<td>{product.CEP}</td>*/}
+                                        {/*<td>{product.CNPJ}</td>*/}
+                                        {/*<td>{product.CNPJ}</td> tentar colocar um botão*/}
+                                    </tr>
+                                );
+                            })} 
+                    </tbody>
+                </Table>
+            </div>
+            <div>
+                <button type="submit"></button>
+            </div>
         </main>
 
         </>
