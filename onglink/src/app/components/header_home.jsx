15979b01480b75624e9066d64efd7dbea6497d00
Rafael Sartori <rafael.costa100@fatec.sp.gov.br>
2025-06-19 22:16:02 -0300

    logo onglink no header home ajustado e estático

--- onglink/src/app/components/header_home.jsx
+++ onglink/src/app/components/header_home.jsx
@@ -1,48 +1,51 @@
 "use client";
-import logo_onglink_01 from '@/src/app/img/LOGO_ONGLINK_1.png'
-import Image from "next/image"
-import Link from 'next/link'
-import {Button, Col, Row} from "react-bootstrap"
+import logo_onglink_01 from "@/src/app/img/LOGO_ONGLINK_1.png";
+import Image from "next/image";
+import Link from "next/link";
+import { Button, Col, Row } from "react-bootstrap";
 import { useRouter } from "next/navigation";
 
+export default function Header_home() {
+  const router = useRouter();
 
+  return (
+    <header className="py-3" id="header_header">
+      <div className="container-fluid d-grid gap-3 align-items-center">
+        <div className="d-flex align-items-center" id="div_header">
+          <Link href="/">
+            <Image
+              className="border"
+              src={logo_onglink_01}
+              style={{ minWidth: "100px" }}
+              alt="Logo ONGLink"
+              width={130}
+              id="logo_onglink"
+            ></Image>
+          </Link>
 
+          <form className="w-100 me-3" role="search"></form>
 
-export default function Header_home () {
-    const router = useRouter();
-    
-    return(
-        <header className="py-3" id="header_header">
-              <div className="container-fluid d-grid gap-3 align-items-center">
-                <div className="d-flex align-items-center" id="div_header">
-                  
-                    <Link href="/">
-                      <Image className="border" src={logo_onglink_01} 
-                          alt="Logo ONGLink"
-                          width={130}
-                          id="logo_onglink">
-                      </Image>
-                    </Link>
-                    
-                  <form className="w-100 me-3" role="search">
-                  </form>
-        
-                  <div id="botoes_cadastro">
-                    <Button 
-                      variant="success"
-                      onClick={() => {router.push("/cad_pj")}}> 
+          <div id="botoes_cadastro">
+            <Button
+              variant="success"
+              onClick={() => {
+                router.push("/cad_pj");
+              }}
+            >
+              Cadastre-se
+            </Button>
 
-                      Cadastre-se</Button>
-
-                    <Button 
-                      variant="success"
-                      onClick={() => {router.push("/login")}}>
-                    
-                    Login</Button>
-                  </div>
-        
-                </div>
-              </div>
-            </header>
-    )
+            <Button
+              variant="success"
+              onClick={() => {
+                router.push("/login");
+              }}
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
